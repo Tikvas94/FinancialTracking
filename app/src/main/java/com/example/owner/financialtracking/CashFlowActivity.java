1dc818f8ef4a0e7f1aad5cccd40b73d9f0a21557
tikvas94 <tikvas94@gmail.com>
2018-01-13 22:29:25 +0200

    fix warnings and firebase issues

--- app/src/main/java/com/example/owner/financialtracking/CashFlowActivity.java
+++ app/src/main/java/com/example/owner/financialtracking/CashFlowActivity.java
@@ -1,20 +1,13 @@
 package com.example.owner.financialtracking;
 
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.ContextMenu;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
@@ -25,22 +18,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import java.util.ArrayList;
-import java.util.Map;
 
-public class CashFlowActivity extends AppCompatActivity implements View.OnClickListener{
+public class CashFlowActivity extends AppCompatActivity {
 
     private FirebaseAnalytics mFirebaseAnalytics;
-
-    private Button Incomebtn , Expensebtn;
-    private Button MyProfile;
-    private FirebaseAuth mAuth;
-    private FirebaseDatabase mFirebaseDatabase;
-    private DatabaseReference myRef;
-    private FirebaseUser user;
     private String userID;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,48 +33,41 @@ public class CashFlowActivity extends AppCompatActivity implements View.OnClickL
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
         // init global
-        Incomebtn = findViewById(R.id.income_btn);
-        Expensebtn = findViewById(R.id.expenses_btn);
-        MyProfile = findViewById(R.id.myProfile);
+        Button incomebtn = findViewById(R.id.income_btn);
+        final Button Expensebtn = findViewById(R.id.expenses_btn);
+        Button myProfile = findViewById(R.id.myProfile);
 
 
-        mAuth=FirebaseAuth.getInstance();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
-        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        FirebaseDatabase mFirebaseDatabase = FirebaseDatabase.getInstance();
 
-        myRef = mFirebaseDatabase.getReference();
+        DatabaseReference myRef = mFirebaseDatabase.getReference();
 
         //get current user
-        user = mAuth.getCurrentUser();
-        userID = user.getUid();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) userID = user.getUid();
+        else userID = "";
 
-        Incomebtn.setOnClickListener(this);
-        Expensebtn.setOnClickListener(this);
-        MyProfile.setOnClickListener(this);
-
-
-        Incomebtn.setOnClickListener(new View.OnClickListener(){
+        incomebtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick( View v){
-                Intent incomeIntent = new Intent(CashFlowActivity.this, IncomeActivity.class);
-                CashFlowActivity.this.startActivity(incomeIntent);
+                handleButtonClick(v, IncomeActivity.class);
             }
         });
 
-        MyProfile.setOnClickListener(new View.OnClickListener(){
+        myProfile.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick( View v){
-                Intent LoadImageIntent = new Intent(CashFlowActivity.this, ProfileActivity.class);
-                CashFlowActivity.this.startActivity(LoadImageIntent);
+                handleButtonClick(v, ProfileActivity.class);
             }
         });
 
 
         Expensebtn.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick( View v){
-                Intent incomeIntent = new Intent(CashFlowActivity.this, ExpensesActivity.class);
-                CashFlowActivity.this.startActivity(incomeIntent);
+            public void onClick(View v){
+                handleButtonClick(v, ExpensesActivity.class);
             }
         });
 
@@ -116,35 +91,29 @@ public class CashFlowActivity extends AppCompatActivity implements View.OnClickL
         });
     }
 
+    static class BackgroundAnalyticsTask extends AsyncTask<Object, Void, Void> {
+        @Override
+        protected Void doInBackground(Object[] objects) {
+            ((FirebaseAnalytics)objects[0]).logEvent((String)objects[1], (Bundle) objects[2]);
+            return null;
+        }
+    }
+
+    private void handleButtonClick(View v, Class<? extends Activity> nextActivity) {
+        // Notify analytics:
+        Bundle params = new Bundle();
+        params.putInt("ButtonID", v.getId());
+        String actName = nextActivity.getSimpleName();
+        new BackgroundAnalyticsTask().execute(mFirebaseAnalytics, actName, params);
+
+        // Navigate to next activity:
+        Intent incomeIntent = new Intent(CashFlowActivity.this, nextActivity);
+        CashFlowActivity.this.startActivity(incomeIntent);
+    }
 
     private boolean isAdmin(DataSnapshot dataSnapshot) {
         dataSnapshot = dataSnapshot.child("Account").child(userID);
-        if(dataSnapshot.child("isManager").getValue().toString().equals("true")){
-
-            return true;
-        }
-        else return false;
-    }
-
-    @Override
-    public void onClick(View view) {
-        Bundle params = new Bundle();
-        params.putInt("ButtonID", view.getId());
-        String btnName;
-
-        switch(view.getId()){
-            case R.id.income_btn:
-                btnName = "Incomebtn";
-                break;
-            case R.id.expenses_btn:
-                btnName = "Expensebtn";
-                break;
-            case R.id.myProfile:
-                btnName = "MyProfile";
-                break;
-            default:
-                btnName = "OtherBtn";
-        }
-        mFirebaseAnalytics.logEvent(btnName, params);
+        Object value = dataSnapshot.child("isManager").getValue();
+        return value != null && "true".equals(value.toString());
     }
 }
